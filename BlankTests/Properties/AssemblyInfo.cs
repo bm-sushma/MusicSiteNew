345ab083ea18cb6601e036a2b57751637feecb7a
selvan123 <selvans@gmail.com>
2018-03-09 13:33:37 +0530

    Add files via upload

---
+++ BlankTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("BlankTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Capgemini")]
+[assembly: AssemblyProduct("BlankTests")]
+[assembly: AssemblyCopyright("Copyright © Capgemini 2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("f4a6c82f-b3d8-421b-8352-75115d71aa4f")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ BlankTests/UnitTest1.cs
@@ -0,0 +1,26 @@
+﻿using BusinessLogicLayer;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+//using System.Web.UI.WebControls;
+
+namespace MusicSiteTests
+{
+    [TestClass]
+    public class Index
+    {
+        [TestMethod]
+        public void bindTopDownloads()
+        {
+            clsCommonHelper objcommon;                    
+            objcommon = new clsCommonHelper();
+            Assert.IsNotNull(objcommon);
+        }
+
+        [TestMethod]
+        public void bindLatestHits()
+        {
+            clsCommonHelper objcommon;     
+            objcommon = new clsCommonHelper();
+            Assert.IsNotNull(objcommon);
+        }
+    }
+}
